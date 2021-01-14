4ae8b43514e4a53c24d8360b0736866a9763794f
RaminHasibi <Ramin.Hasibi94@gmail.com>
2021-01-14 18:47:03 +0100

    1.0.0

---
+++ pdp_utils/Utils.py
@@ -0,0 +1,222 @@
+import numpy as np
+from collections import namedtuple
+
+
+def load_problem(filename):
+    """
+
+    :rtype: object
+    :param filename: The address to the problem input file
+    :return: named tuple object of the problem attributes
+    """
+    A = []
+    B = []
+    C = []
+    D = []
+    E = []
+    with open(filename) as f:
+        lines = f.readlines()
+        num_nodes = int(lines[1])
+        num_vehicles = int(lines[3])
+        num_calls = int(lines[num_vehicles + 5 + 1])
+
+        for i in range(num_vehicles):
+            A.append(lines[1 + 4 + i].split(','))
+
+        for i in range(num_vehicles):
+            B.append(lines[1 + 7 + num_vehicles + i].split(','))
+
+        for i in range(num_calls):
+            C.append(lines[1 + 8 + num_vehicles * 2 + i].split(','))
+
+        for j in range(num_nodes * num_nodes * num_vehicles):
+            D.append(lines[1 + 2 * num_vehicles + num_calls + 9 + j].split(','))
+
+        for i in range(num_vehicles * num_calls):
+            E.append(lines[1 + 1 + 2 * num_vehicles + num_calls + 10 + j + i].split(','))
+        f.close()
+
+    Cargo = np.array(C, dtype=np.double)[:, 1:]
+    D = np.array(D, dtype=np.int)
+
+    TravelTime = np.zeros((num_vehicles + 1, num_nodes + 1, num_nodes + 1))
+    TravelCost = np.zeros((num_vehicles + 1, num_nodes + 1, num_nodes + 1))
+    for j in range(len(D)):
+        TravelTime[D[j, 0]][D[j, 1], D[j, 2]] = D[j, 3]
+        TravelCost[D[j, 0]][D[j, 1], D[j, 2]] = D[j, 4]
+
+    VesselCapacity = np.zeros(num_vehicles)
+    StartingTime = np.zeros(num_vehicles)
+    FirstTravelTime = np.zeros((num_vehicles, num_nodes))
+    FirstTravelCost = np.zeros((num_vehicles, num_nodes))
+    A = np.array(A, dtype=np.int)
+    for i in range(num_vehicles):
+        VesselCapacity[i] = A[i, 3]
+        StartingTime[i] = A[i, 2]
+        for j in range(num_nodes):
+            FirstTravelTime[i, j] = TravelTime[i + 1, A[i, 1], j + 1] + A[i, 2]
+            FirstTravelCost[i, j] = TravelCost[i + 1, A[i, 1], j + 1]
+    TravelTime = TravelTime[1:, 1:, 1:]
+    TravelCost = TravelCost[1:, 1:, 1:]
+    VesselCargo = np.zeros((num_vehicles, num_calls + 1))
+    B = np.array(B, dtype=object)
+    for i in range(num_vehicles):
+        VesselCargo[i, np.array(B[i], dtype=np.int)] = 1
+    VesselCargo = VesselCargo[:, 1:]
+
+    LoadingTime = np.zeros((num_vehicles + 1, num_calls + 1))
+    UnloadingTime = np.zeros((num_vehicles + 1, num_calls + 1))
+    PortCost = np.zeros((num_vehicles + 1, num_calls + 1))
+    E = np.array(E, dtype=np.int)
+    for i in range(num_vehicles * num_calls):
+        LoadingTime[E[i, 0], E[i, 1]] = E[i, 2]
+        UnloadingTime[E[i, 0], E[i, 1]] = E[i, 4]
+        PortCost[E[i, 0], E[i, 1]] = E[i, 5] + E[i, 3]
+
+    LoadingTime = LoadingTime[1:, 1:]
+    UnloadingTime = UnloadingTime[1:, 1:]
+    PortCost = PortCost[1:, 1:]
+    output = {
+        'n_nodes': num_nodes,
+        'n_vehicles': num_vehicles,
+        'n_calls': num_calls,
+        'Cargo': Cargo,
+        'TravelTime': TravelTime,
+        'FirstTravelTime': FirstTravelTime,
+        'VesselCapacity': VesselCapacity,
+        'LoadingTime': LoadingTime,
+        'UnloadingTime': UnloadingTime,
+        'VesselCargo': VesselCargo,
+        'TravelCost': TravelCost,
+        'FirstTravelCost': FirstTravelCost,
+        'PortCost': PortCost
+    }
+    return output
+
+
+def feasibility_check(solution, problem):
+    """
+
+    :rtype: tuple
+    :param solution: The input solution of order of calls for each vehicle to the problem
+    :param problem: The pickup and delivery problem object
+    :return: whether the problem is feasible and the reason for probable infeasibility
+    """
+    num_vehicles = problem['n_vehicles']
+    Cargo = problem['Cargo']
+    TravelTime = problem['TravelTime']
+    FirstTravelTime = problem['FirstTravelTime']
+    VesselCapacity = problem['VesselCapacity']
+    LoadingTime = problem['LoadingTime']
+    UnloadingTime = problem['UnloadingTime']
+    VesselCargo = problem['VesselCargo']
+    solution = np.append(solution, [0])
+    ZeroIndex = np.array(np.where(solution == 0)[0], dtype=int)
+    feasibility = True
+    tempidx = 0
+    c = 'Feasible'
+    for i in range(num_vehicles):
+        currentVPlan = solution[tempidx:ZeroIndex[i]]
+        currentVPlan = currentVPlan - 1
+        NoDoubleCallOnVehicle = len(currentVPlan)
+        tempidx = ZeroIndex[i] + 1
+        if NoDoubleCallOnVehicle > 0:
+
+            if not np.all(VesselCargo[i, currentVPlan]):
+                feasibility = False
+                c = 'incompatible vessel and cargo'
+                break
+            else:
+                LoadSize = 0
+                currentTime = 0
+                sortRout = np.sort(currentVPlan)
+                I = np.argsort(currentVPlan)
+                Indx = np.argsort(I)
+                LoadSize -= Cargo[sortRout, 2]
+                LoadSize[::2] = Cargo[sortRout[::2], 2]
+                LoadSize = LoadSize[Indx]
+                if np.any(VesselCapacity[i] - np.cumsum(LoadSize) < 0):
+                    feasibility = False
+                    c = 'Capacity exceeded'
+                    break
+                Timewindows = np.zeros((2, NoDoubleCallOnVehicle))
+                Timewindows[0] = Cargo[sortRout, 6]
+                Timewindows[0, ::2] = Cargo[sortRout[::2], 4]
+                Timewindows[1] = Cargo[sortRout, 7]
+                Timewindows[1, ::2] = Cargo[sortRout[::2], 5]
+
+                Timewindows = Timewindows[:, Indx]
+
+                PortIndex = Cargo[sortRout, 1].astype(int)
+                PortIndex[::2] = Cargo[sortRout[::2], 0]
+                PortIndex = PortIndex[Indx] - 1
+
+                LU_Time = UnloadingTime[i, sortRout]
+                LU_Time[::2] = LoadingTime[i, sortRout[::2]]
+                LU_Time = LU_Time[Indx]
+                Diag = TravelTime[i, PortIndex[:-1], PortIndex[1:]]
+                FirstVisitTime = FirstTravelTime[i, int(Cargo[currentVPlan[0], 0] - 1)]
+
+                RouteTravelTime = np.hstack((FirstVisitTime, Diag.flatten()))
+
+                ArriveTime = np.zeros(NoDoubleCallOnVehicle)
+                for j in range(NoDoubleCallOnVehicle):
+                    ArriveTime[j] = np.max((currentTime + RouteTravelTime[j], Timewindows[0, j]))
+                    if ArriveTime[j] > Timewindows[1, j]:
+                        feasibility = False
+                        c = 'Time window exceeded at call {}'.format(j)
+                        break
+                    currentTime = ArriveTime[j] + LU_Time[j]
+
+    return feasibility, c
+
+
+def cost_function(Solution, problem):
+    """
+
+    :param Solution: the proposed solution for the order of calls in each vehicle
+    :param problem:
+    :return:
+    """
+
+    num_vehicles = problem['n_vehicles']
+    Cargo = problem['Cargo']
+    TravelCost = problem['TravelCost']
+    FirstTravelCost = problem['FirstTravelCost']
+    PortCost = problem['PortCost']
+
+
+    NotTransportCost = 0
+    RouteTravelCost = np.zeros(num_vehicles)
+    CostInPorts = np.zeros(num_vehicles)
+
+    Solution = np.append(Solution, [0])
+    ZeroIndex = np.array(np.where(Solution == 0)[0], dtype=int)
+    tempidx = 0
+
+    for i in range(num_vehicles + 1):
+        currentVPlan = Solution[tempidx:ZeroIndex[i]]
+        currentVPlan = currentVPlan - 1
+        NoDoubleCallOnVehicle = len(currentVPlan)
+        tempidx = ZeroIndex[i] + 1
+
+        if i == num_vehicles:
+            NotTransportCost = np.sum(Cargo[currentVPlan, 3]) / 2
+        else:
+            if NoDoubleCallOnVehicle > 0:
+                sortRout = np.sort(currentVPlan)
+                I = np.argsort(currentVPlan)
+                Indx = np.argsort(I)
+
+                PortIndex = Cargo[sortRout, 1].astype(int)
+                PortIndex[::2] = Cargo[sortRout[::2], 0]
+                PortIndex = PortIndex[Indx] - 1
+
+                Diag = TravelCost[i, PortIndex[:-1], PortIndex[1:]]
+
+                FirstVisitCost = FirstTravelCost[i, int(Cargo[currentVPlan[0], 0] - 1)]
+                RouteTravelCost[i] = np.sum(np.hstack((FirstVisitCost, Diag.flatten())))
+                CostInPorts[i] = np.sum(PortCost[i, currentVPlan]) / 2
+
+    TotalCost = NotTransportCost + sum(RouteTravelCost) + sum(CostInPorts)
+    return TotalCost
